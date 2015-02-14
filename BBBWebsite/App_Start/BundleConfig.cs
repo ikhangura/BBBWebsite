c73fa64358d6351c7920a1d167412a10dd583aae
alan_lai <alan@ergostratus.com>
2015-02-13 21:12:43 -0800

    Hooked up Foundation CSS & JS Also replaced _Layout and Home page with foundation

--- BBBWebsite/App_Start/BundleConfig.cs
+++ BBBWebsite/App_Start/BundleConfig.cs
@@ -19,12 +19,20 @@ namespace BBBWebsite
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Scripts/bootstrap.js",
+            //          "~/Scripts/respond.js"));
+
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/bootstrap.css",
+            //          "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/foundation").Include(
+          "~/Scripts/foundation/foundation.min.js",
+          "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/foundation/foundation.css",
                       "~/Content/site.css"));
         }
     }
