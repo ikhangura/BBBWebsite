dc776c869943d7c55c2f5361497c6e9ebbc8e058
alan_lai <alan@ergostratus.com>
2015-04-07 01:17:57 -0700

    Basic article view complete. Currently only HTML with no angular hooks yet

--- AngularBBB/article/articleController.js
+++ AngularBBB/article/articleController.js
@@ -2,13 +2,26 @@
 
     var app = angular.module("myApp");
 
-    var articleController = function ($scope, $routeParams) {
+    var articleController = function ($scope, $http, $cookie, $routeParams) {
         //interactive functionality code goes here
         $scope.newsid = $routeParams.newsid;
 
+        var userInfo = JSON.parse($cookie.userData);
+        var token = userInfo.token;
+        var usertype = userInfo.type;
+        var userid = userInfo.userid;
+        var userrealname = userInfo.name;
 
+        var loadArticle = function () {
 
+        };
+
+        var loadComments = function () {
+
+        }
+
+        $scope.userinfo = "ID:" + userid + " " + userrealname + " " + usertype + " " + token;
     }
 
-    app.controller("articleController", ["$scope", "$routeParams", articleController]);
+    app.controller("articleController", ["$scope", "$http", "$cookies", "$routeParams", articleController]);
 }());
